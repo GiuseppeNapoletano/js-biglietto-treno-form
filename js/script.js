39b9395cedda9d95e418075b94aec0fa2d543ba6
GiuseppeNapoletano <andreanap04@gmail.com>
2025-03-04 18:09:50 +0100

    feat: work done

---
+++ js/script.js
@@ -0,0 +1,36 @@
+/*Il programma dovrà mostrare una form da compilare usando i corretti campi HTML di input con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Utilizzate Bootstrap come libreria UI.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).Vi consigliamo di seguire questo ordine nella risoluzione dell'esercizio:
+1. Preparazione dell'interfaccia utente con utilizzo di classi di bootstrap.
+2. Al click sul bottone "calcola" prelevare tutti i dati dagli input e stamparli in console.
+3. Eseguire il calcolo del prezzo e stamparlo in console.
+4. Stampare il risultato in pagina.*/
+
+const ticketform = document.getElementById("ticketForm");
+const etaInput = document.getElementById("eta");
+const kmInput = document.getElementById("km");
+const risultatoElemento = document.getElementById("risultato");
+ticketform.addEventListener("submit", gestisciForm);
+
+
+
+function gestisciForm(event) {
+    event.preventDefault()
+    const eta = parseInt(etaInput.value);
+    const km = parseInt(kmInput.value);
+    const prezzoBigliettoIntero = km * 0.21;
+    const scontoMinorenni = prezzoBigliettoIntero * 20 / 100;
+    const scontoOver = prezzoBigliettoIntero * 40 / 100;
+    let prezzoBigliettoFinale
+    if (eta <= 17) {
+        prezzoBigliettoFinale = prezzoBigliettoIntero - scontoMinorenni;
+    } else if (eta >= 65) {
+        prezzoBigliettoFinale = prezzoBigliettoIntero - scontoOver;
+    } else prezzoBigliettoFinale = prezzoBigliettoIntero;
+    risultatoElemento.innerText = prezzoBigliettoFinale.toFixed(2);
+}
+
+
